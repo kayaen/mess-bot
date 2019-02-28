719451110af15f099171f0ebbb3f8f131afbd5b0
kayaen <kayaengn@gmail.com>
2019-02-28 14:53:54 +0900

    respond with same message

--- app.py
+++ app.py
@@ -40,12 +40,14 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    response_sent_text = message['message']['text']
+                    #response_sent_text = get_message()
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                    #response_sent_nontext = get_message()
+                    #send_message(recipient_id, response_sent_nontext)
+                    bot.send_attachment_url(recipient_id, message['message']['attachment']['type'], message['message']['attachment']['payload']['url'])
     return "Message Processed"
 
 
